f7a54ff0b07a83b0ce246d6d4757c1bcd1c782b2
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-04 05:55:04 +0530

    update

--- config/db.php
+++ config/db.php
@@ -1,11 +1,10 @@
 <?php
 	// connect to database
 	$db = mysqli_connect("localhost", "root", "", "myphpblog");
-
 	if (!$db) {
 		die("Error connecting to database: " . mysqli_connect_error());
 	}
     // define global constants
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	define('ROOT_URL', 'http://localhost:5050/myPhpBlog/');
+	define('ROOT_URL', 'http://local.rocket.co.in:8081/test1/phpmyblog/');
 ?>
